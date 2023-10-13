80902a0749a81a2c9d2a4b89893a288155975d19
Wesley Unwin <wes_unwin@hotmail.com>
2023-10-12 23:07:07 -0400

    examples page: add description about building library first

--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,10 @@ const demos = [
     { name: '1. Hello World!', run: runDemo1 }
 ];
 
+const desc = document.createElement('p');
+desc.innerHTML = 'Welcome to the examples page! <br /> Please build the library by running "npm run build" first, repeat this if making src code changes. <br /> Then run this page via: "npm run examples".'
+document.body.appendChild(desc);
+
 demos.forEach(demo => {
     const button = document.createElement('button');
     button.textContent = demo.name;
