17b3b45efb34552b45a7dea4905ad253956d3671
Wesley Unwin <wes_unwin@hotmail.com>
2023-10-22 14:35:51 -0400

    index.ts: export underlying libraries

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@ import _Game from './Game'
 import _Scene from './Scene'
 import _GameObject from './GameObject'
 
+import * as _THREE from 'three';
+import _RAPIER from '@dimforge/rapier3d-compat';
+import _ThreeMeshUI from 'three-mesh-ui'
+
 // This file defines the API of this library.
 // Only export things that should be publicly/directly exposed to applications.
 
 export const Game = _Game
 export const Scene = _Scene
 export const GameObject = _GameObject
+
+// Underlying libraries:
+export const THREE = _THREE;
+export const RAPIER = _RAPIER
+export const ThreeMeshUI = _ThreeMeshUI;
