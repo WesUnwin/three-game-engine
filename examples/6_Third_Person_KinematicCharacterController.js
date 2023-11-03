dd0860952bb1fa6a89a3a4d21f83e534fa1e3b1c
Wesley Unwin <wes_unwin@hotmail.com>
2023-11-02 22:01:16 -0400

    Create example 6 - third person character controller

---
+++ examples/6_Third_Person_KinematicCharacterController.js
@@ -0,0 +1,60 @@
+import { Game, KinematicCharacterController } from "../dist/index";
+import TestAreaScene from "./scenes/TestAreaScene";
+
+const runDemo = async () => {
+    const game = new Game({
+      rendererOptions: {
+        setupFullScreenCanvas: true
+      },
+      assetOptions: {
+        baseURL: 'http://localhost:8080/assets'
+      }
+    })
+
+    class ExampleCharacter extends KinematicCharacterController {
+        constructor(parent, options) {
+            super(parent,
+              { // GameObjectOptions
+                models: [],
+                ...options
+              }, 
+              { // CharacterControllerOptions
+              }, 
+              { // KinematicCharacterControllerOptions
+                autoStep: {
+                  maxHeight: 0.35, // automatically step onto platforms as long as their not taller than this value
+                  minWidth: 0.5, // in order to auto-step onto, at least this much clearance is needed on top of it
+                  includeDynamicBodies: false // if true this would step onto dynamic bodies (that are small enough)
+                },
+                applyImpulsesToDynamicBodies: true // allows you to push around things like the Barrel and Bale of Hay
+              }
+            )
+        }
+    }
+  
+    const scene = new TestAreaScene();
+
+    const character = new ExampleCharacter(scene, {
+        name: 'player',
+        position: { x: -3, y: 3, z: 3 }
+    });
+
+
+
+    scene.showPhysics();
+
+    await game.loadScene(scene);
+
+    game.play();
+
+    const player = scene.findByName('player');
+
+    const cam = game.renderer.getCamera();
+    cam.position.set(-11,5,11);
+
+    cam.lookAt(player.threeJSGroup.position)
+
+    window.game = game;
+}
+
+export default runDemo
--- examples/index.js
+++ examples/index.js
@@ -3,13 +3,15 @@ import runDemo2 from "./2_Creating_GameObjects";
 import runDemo3 from "./3_Moving_GameObjects";
 import runDemo4 from "./4_GameObject_Physics";
 import runDemo5 from "./5_First_Person_KinematicCharacterController";
+import runDemo6 from "./6_Third_Person_KinematicCharacterController";
 
 const demos = [
     { name: '1. Hello World!', run: runDemo1 },
     { name: '2. Creating Game Objects', run: runDemo2 },
     { name: '3. Moving Game Objects', run: runDemo3 },
     { name: '4. GameObject Physics', run: runDemo4 },
-    { name: '5. CharacterController', run: runDemo5 }
+    { name: '5. First Person CharacterController', run: runDemo5 },
+    { name: '6. Third Person CharacterController', run: runDemo6 }
 ];
 
 const desc = document.createElement('p');
