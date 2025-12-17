e23a8bd27a884df40550ebf9f8563efc02f1d652
Wesley Unwin <wes_unwin@hotmail.com>
2025-12-17 16:41:04 -0500

    SoundComponent: logic to display and edit all properties

--- scene_editor/src/Sidebar/SelectedItem/Components/SoundComponent.jsx
+++ scene_editor/src/Sidebar/SelectedItem/Components/SoundComponent.jsx
@@ -1,17 +1,89 @@
 import React from 'react';
-import TreeView from '../../Hierarchy/TreeView.jsx';
 import { FaTrash } from 'react-icons/fa';
+import Property from '../Property.jsx';
+import TreeView from '../../Hierarchy/TreeView.jsx';
+import PropertyList from '../PropertyList.jsx';
+import NumberInput from '../NumberInput.jsx';
+import ColorInput from '../ColorInput.jsx';
 
-const SoundComponent = ({ componentJSON, onRemove }) => {
+const commonProperties = [
+  { name: 'assetPath', type: 'text', default: '', disabled: true },
+  { name: 'loop', type: 'boolean', default: false },
+  { name: 'autoplay', type: 'boolean', default: false },
+  { name: 'volume', type: 'number', default: 1.0 },
+  { name: 'playbackRate', type: 'number', default: 1 }
+];
+
+const positionalAudioProperties = [
+  { name: 'refDistance', type: 'number', default: 1 },
+  { name: 'rolloffFactor', type: 'number', default: 1 },
+  { name: 'distanceModel', type: 'text', default: 'linear' },
+  { name: 'maxDistance', type: 'number', default: 10000 }
+];
+
+const SoundComponent = ({ componentJSON, onChange, onRemove, audioType = "PositionalAudio" }) => {
+  const onChangeProperty = (field, value) => {
+    onChange({ ...componentJSON, [field]: value });
+  };
+
+  const audioTypeProperties = {
+    Audio: commonProperties,
+    PositionalAudio: commonProperties.concat(positionalAudioProperties)
+  }
+
+  const properties = audioTypeProperties[audioType];
+
+  if (!properties) {
+    throw new Error(`SoundComponent: invalid audioType: ${audioType}`);
+  }
+
   return (
     <TreeView
-      label={`Sound: ${componentJSON.assetPath}`}
+      label={componentJSON.name}
+      expandOnClick={true}
       actions={[
-        { icon: <FaTrash />, onClick: onRemove }
+        { icon: <FaTrash />, onClick: onRemove },  
       ]}
-      onClick={() => {}}
-    />
-  );
+    >
+      <PropertyList>
+        {properties.map(prop => {
+          const value = prop.name in componentJSON ? componentJSON[prop.name] : prop.default
+
+          return (
+            <Property key={prop.name} label={`${prop.name}:`}>
+            {prop.type === 'number' ? (
+              <NumberInput
+                value={value}
+                onChange={val => onChangeProperty([prop.name], val)}
+              />
+            ) : prop.type === 'boolean' ? (
+              <input
+                type="checkbox"
+                checked={value}
+                onChange={() => onChangeProperty([prop.name], !value)}
+              />
+            ) : prop.type === 'color' ? (
+              <ColorInput
+                value={value}
+                onChange={val => onChangeProperty([prop.name], val)}
+              />
+            ) : prop.type === 'text' ? (
+              <input
+                type="text"
+                value={value}
+                placeholder={prop.placeholder}
+                onChange={event => onChangeProperty([prop.name], event.target.value)}
+                disabled={typeof prop.disabled === 'boolean' ? prop.disabled : false}
+              />
+            ) : (
+              'INVALID PROP TYPE'
+            )}
+            </Property>
+          );
+        })}
+      </PropertyList>
+    </TreeView>
+  )
 };
 
 export default SoundComponent;
